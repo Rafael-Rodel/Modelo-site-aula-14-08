049f918a5c72132aaee7eda538853c97b12b7d17
jpf <juliano2004r7.com@gmail.com>
2026-08-14 22:12:48 -0300

    funcional

---
+++ include/header.php
@@ -0,0 +1,41 @@
+<?php
+
+$menu = [
+    "Home" => "index.php",
+    "Marketing" => "marketing.php",
+    "Internet" => "internet.php",
+    "Ganhar Dinheiro" => "ganhar-dinheiro.php",
+    "Webmaster" => "webmaster.php",
+    "Scripts" => "scripts.php",
+    "Software" => "software.php",
+    "Comércio Eletrônico" => "comercio-eletronico.php",
+    "Downloads" => "downloads.php",
+    "Contato" => "contato.php"
+];
+
+?>
+
+<header class="header">
+
+    <div class="header-topo">
+    </div>
+
+    <nav class="navbar">
+
+        <ul>
+
+            <?php foreach ($menu as $nome => $link): ?>
+
+                <li>
+                    <a href="<?= $link ?>">
+                        <?= $nome ?>
+                    </a>
+                </li>
+
+            <?php endforeach; ?>
+
+        </ul>
+
+    </nav>
+
+</header>
