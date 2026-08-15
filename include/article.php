23ca8383b1b6d941309d149d51426739aa94ef13
Arthur Wilson <arthur.wilson.oficial@gmail.com>
2026-08-14 22:16:36 -0300

    #6 Destaque do article
    - feito section card do destaque completo;
    - adionado imagem no assets

---
+++ include/article.php
@@ -0,0 +1,32 @@
+<section>
+
+    <div class="cardDestaque">
+
+        <h3 class="destaque">DESTAQUES</h3>
+
+        <div class="contentDestaque">
+
+            <div class="imagemDestaque">
+                <img src="./assets/corvoCard01.png" alt="">
+            </div>
+
+            <div class="textDestaque">
+
+                <h4>Corvos são a nova tendência!</h4>
+
+                <p>
+                    Os corvos estão conquistando cada vez mais espaço e se
+                    tornando a nova tendência! Misteriosos, inteligentes e
+                    cheios de personalidade, eles chegaram para transformar
+                    o visual com um toque sombrio e fascinante.
+                </p>
+
+                <a href="#">LEIA MAIS</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
