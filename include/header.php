7e34afef5971b6b20ff75439fc8126bb0e22ba76
Lucas Milhão <lucasmilhao683@gmail.com>
2026-08-19 22:29:29 -0300

    Fazer a aba noticias

--- include/header.php
+++ include/header.php
@@ -14,7 +14,7 @@ $menu = [
 ];
 
 ?>
-
+<link rel="stylesheet" href="assets/style.css">
 <header class="header">
 
     <div class="header-topo">
---
+++ noticia.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template Basico</title>
+    <link rel="stylesheet" href="./noticia.css">
+</head>
+
+<body>
+    <?php
+    include_once("./include/header.php")
+        ?>
+    <main>
+        <div class="primeira-pagina">
+            <h1>
+                <?php
+                $artigo = $_GET['artigo'];
+                echo $artigo;
+                ?>
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolores expedita tenetur corrupti ab
+                doloribus modi velit</p>
+            <img src="https://visitrio.com.br/wp-content/uploads/2024/10/por-do-sol-no-rio.jpg" alt="">
+        </div>
+        <div class="paragrafos">
+            <div class="texto1">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolor, suscipit sunt magni odit
+                    praesentium ea atque explicabo at nostrum velit quas vero dolorem excepturi architecto. Omnis
+                    dolorum id cumque.
+                    Perspiciatis iusto exercitationem hic iste eos repudiandae omnis, soluta aut asperiores, accusantium
+                    fuga cumque! Sapiente sunt ipsam suscipit accusantium quam quasi, enim totam, earum vitae amet
+                    recusandae similique blanditiis sint.
+                </p>
+            </div>
+            <div class="texto2">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus sequi quod? Ea nesciunt
+                    quasi itaque culpa sapiente officiis, pariatur, optio accusamus veniam cumque exercitationem
+                    provident facere aliquid rem molestiae.
+                    Corrupti architecto incidunt ex consequuntur maxime possimus sapiente non autem facere, tenetur
+                    ipsum, recusandae molestias nesciunt tempore earum reprehenderit! Minima enim architecto aliquid
+                    neque quam velit commodi officiis soluta delectus.
+                </p>
+            </div>
+        </div>
+    </main>
+
+    <?php
+    include_once('./include/footer.php');
+    ?>
+    <script src="./sript.js"></script>
+</body>
+
+</html>
